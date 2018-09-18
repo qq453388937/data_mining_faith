f70bbb1e9a26248bc35242300563c973cd38baeb
camelia-peixiaodong <453388937@qq.com>
2018-09-18 23:23:20 +0800

    init

---
+++ 1.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+import sys
+import matplotlib.pyplot as plt
+import numpy as  np
+
+# x = [1, 2, 3, 4]
+# y = [10, 20, 30, 40]
+# plt.plot(x, y)
+# plt.show()
+# a = [2, 3, 4, 1, 5]
+# """
+# #冒泡排序要排序n个数，由于每遍历一趟只排好一个数字，
+#        #则需要遍历n-1趟，所以最外层循环是要循环n-1次，而
+#         #每次趟遍历中需要比较每归位的数字，则要在n-1次比较
+#         #中减去已排好的i位数字，则第二层循环要遍历是n-1-i次
+# """
+# for i in range(len(a) - 1):
+#     for j in range(len(a) - 1 - i):
+#         if a[j] > a[j + 1]:
+#             a[j], a[j + 1] = a[j + 1], a[j]
+
+# print(a)
+
+# res =numpy.array([1,2,3],dtype='i8')
+# print(res.dtype)
+# print(res.flags)
